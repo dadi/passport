7d42771d31ace2bb1db4e6e187252586180bafa8
Eduardo Bouças <mail@eduardoboucas.com>
2016-02-05 13:15:17 +0000

    Add request agent option

--- index.js
+++ index.js
@@ -1,49 +1,88 @@
 var request = require('request-promise');
 
-module.exports = (function (data) {
-  var walletModule;
-  var requestNewToken = function () {
+module.exports = (function (data, requestAgent) {
+  var Passport = function (data, requestAgent) {
+    this.data = data;
+    this.requestAgent = requestAgent;
+
+    if (typeof data.wallet === 'function') {
+      this.walletModule = data.wallet;
+    } else if ((typeof data.wallet === 'string') && (data.wallet !== 'none')) {
+      this.walletModule = require(__dirname + '/wallets/' + data.wallet);
+    }
+  };
+
+  Passport.prototype.requestToken = function () {
     return request({
       json: true,
       method: 'POST',
       uri: data.uri + '/token',
       body: data.credentials
-    }).then(function (response) {
-      if (wallet) {
-        wallet.write(response);  
+    }).then((function (response) {
+      if (this.wallet) {
+        this.wallet.write(response);
       }
 
-      return Promise.resolve(response.accessToken);
-    });
+      return Promise.resolve(this.return(response.accessToken));
+    }).bind(this));
   };
-  
-  if (typeof data.wallet === 'string') {
-    if (data.wallet === 'none') {
-      return requestNewToken();
+
+  Passport.prototype.return = function (token) {
+    if (this.requestAgent) {
+      return function () {
+        var requestOptions = {};
+
+        if (typeof arguments[0] === 'string') {
+          // The first pararameter is a URL string
+          requestOptions.url = arguments[0];
+          requestOptions.headers = {
+            'Authorization': 'Bearer ' + token
+          }
+        } else {
+          requestOptions = arguments[0];
+
+          // The first argument is an options object
+          if ('headers' in requestOptions) {
+            requestOptions.headers['Authorization'] = 'Bearer ' + token;
+          } else {
+            requestOptions.headers = {
+              'Authorization': 'Bearer ' + token
+            }
+          }
+        }
+
+        arguments[0] = requestOptions;
+
+        return requestAgent.apply(this, arguments);
+      };
     } else {
-      walletModule = require(__dirname + '/wallets/' + data.wallet);
+      return token;
     }
-  } else {
-    walletModule = data.wallet;
-  }
+  };
 
-  if (!walletModule) {
-    return requestNewToken();
-  }
-
-  var Wallet = walletModule;
-  var wallet = new Wallet(data.walletOptions);
-
-  try {
-    var token = wallet.read();
-    var currentDate = Math.floor(Date.now() / 1000);
-
-    if (token.expirationDate > currentDate) {
-      return Promise.resolve(token.accessToken);
-    } else {
-      return requestNewToken();
+  Passport.prototype.get = function () {
+    if (!this.walletModule) {
+      return this.requestToken();
     }
-  } catch (e) {
-    return requestNewToken();
-  }
+
+    var Wallet = this.walletModule;
+    this.wallet = new Wallet(this.data.walletOptions);
+
+    try {
+      var token = this.wallet.read();
+      var currentDate = Math.floor(Date.now() / 1000);
+
+      if (token.expirationDate > currentDate) {
+        return Promise.resolve(this.return(token.accessToken));
+      } else {
+        return this.requestToken();
+      }
+    } catch (e) {
+      return this.requestToken();
+    }
+  };
+
+  var passport = new Passport(data, requestAgent);
+
+  return passport.get();
 });
