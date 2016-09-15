c6e874827ff8c5b53f38f6eb39792766312e24c4
Eduardo Bouças <mail@eduardoboucas.com>
2016-09-15 11:52:28 +0100

    fix: ensure wallet directory exists before writing (closes #27)

--- node/src/wallets/file.js
+++ node/src/wallets/file.js
@@ -1,28 +1,32 @@
-var fs = require('fs');
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var path = require('path')
 
 var FileWallet = function (options) {
-  this.path = options.path;
-};
+  this.path = path.resolve(options.path)
+}
 
 FileWallet.prototype.read = function () {
-  var rawContent = fs.readFileSync(this.path, 'utf8');
+  var rawContent = fs.readFileSync(this.path, 'utf8')
   
   try {
-    var parsedContent = JSON.parse(rawContent);
+    var parsedContent = JSON.parse(rawContent)
 
-    return parsedContent;
+    return parsedContent
   } catch (e) {
-    return false;
+    return false
   }
-};
+}
 
 FileWallet.prototype.write = function (data) {
   var content = {
     accessToken: data.accessToken,
     expirationDate: Math.floor(Date.now() / 1000) + data.expiresIn
-  };
+  }
 
-  fs.writeFileSync(this.path, JSON.stringify(content), 'utf8');
-};
+  mkdirp(path.dirname(this.path), (err) => {
+    fs.writeFileSync(this.path, JSON.stringify(content), 'utf8')
+  })
+}
 
-module.exports = FileWallet;
+module.exports = FileWallet
